9df219ddc1b3bde5e34e74db4990f00b367a3cff
n2ptune <n2ptune@outlook.com>
2019-11-15 11:42:55 +0900

    test: dynamic route in now

---
+++ client/pages/articles/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :style="{ fontSize: '5rem', marginTop: '64px' }">
+    {{ aid }}
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ params }) {
+    return {
+      aid: params.id
+    }
+  }
+}
+</script>
